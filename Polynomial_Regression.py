e47ac132c49de8ef62d459f0913d1c97e1ddd7e9
JaneEvans <jinjin.ge@utexas.edu>
2016-08-09 12:41:02 -0700

    Add Polynomial_Regression 	Using numpy, sklearn, sklearn.preprocessing

---
+++ Polynomial_Regression.py
@@ -0,0 +1,42 @@
+#!/bin/python3
+
+import sys
+
+def predictOfficePrice(_trainList, _testList):
+	import numpy as np
+	train = np.array(_trainList)
+	trainData = train[: , 0:F]
+	trainValue = train[:, F]
+	testData = np.array(_testList)
+
+	from sklearn.preprocessing import PolynomialFeatures
+	from sklearn import linear_model
+	poly = PolynomialFeatures(degree = 3)
+	trainData_ = poly.fit_transform(trainData)
+	testData_ = poly.fit_transform(testData)
+
+	clf = linear_model.LinearRegression()
+	clf.fit(trainData_, trainValue)
+
+	return clf.predict(testData_)
+
+_data = []
+trainList = []
+testList = []
+
+F, N = input().strip().split(" ") #F is the number of observed features. 
+F, N = int(F), int(N)			  #N is the train sample size
+for i in range(N):
+    rowData = input().strip()
+    _data=rowData.split(" ")
+    trainList.append(list(map(float,_data)))
+
+M = int(input())				  #M is the test sample size
+for i in range(M):
+    rowData = input().strip()
+    _data=rowData.split(" ")
+    testList.append(list(map(float,_data)))
+
+for testValue in predictOfficePrice(trainList, testList):
+    print(rount(testValue,2))
+
